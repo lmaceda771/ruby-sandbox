8a0f1b6c505e9b6017e6e4deffa514eac71eb792
lmaceda771 <143520701+lmaceda771@users.noreply.github.com>
2023-12-12 15:38:12 +0000

    added gems

--- howdy.rb
+++ howdy.rb
@@ -1,3 +1,13 @@
-# here's a comment
+# /howdy.rb
+source "https://rubygems.org"
+ 
+gem "activesupport"
+gem "awesome_print"
+gem "pry-byebug"
+
+
 my_string = "Hello, World!"
 pp my_string
+
+
+require "./goodbye.rb"
